e0edc36471435b2134bb446e9943a3b1296fb039
mjvolk3 <mjvolk3>
2023-08-02 14:49:39 -0500

    init import trick

---
+++ src/torchcell/sequence/__init__.py
@@ -0,0 +1,8 @@
+# src/torchcell/sequence/__init__.py
+from torchcell.sequence.sequence import (
+    AbcGenome,
+    DnaSelectionResult,
+    mismatch_positions,
+    get_chr_from_description,
+    roman_to_int,
+)

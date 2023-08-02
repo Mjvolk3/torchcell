e044e67e5c002930940674b91811a18c8bd27e14
mjvolk3 <mjvolk3>
2023-08-02 18:47:29 -0500

    make mito first chr index 0 in contansts

--- src/torchcell/sgd/sequence/constants.py
+++ src/torchcell/sgd/sequence/constants.py
@@ -1,6 +1,7 @@
 # can get from [print(chr.chrom) for chr in self.db.features_of_type("chromosome")]
 # or genome.db.fatures_of_type("chromosome")
 CHROMOSOMES = [
+    "chrmt",  # We put MT at 0, because it is circular, and this preserves arabic to roman
     "chrI",
     "chrII",
     "chrIII",
@@ -17,7 +18,6 @@ CHROMOSOMES = [
     "chrXIV",
     "chrXV",
     "chrXVI",
-    "chrmt",
 ]
 
 if __name__ == "__main__":

e4a4fa5d3ad4a833cae66b179b95094d5c50ceb2
mjvolk3 <mjvolk3>
2023-09-13 02:05:20 -0500

    dcell model start

---
+++ src/torchcell/models/dcell.py
@@ -0,0 +1,8 @@
+# src/torchcell/models/dcell.py
+# [[src.torchcell.models.dcell]]
+# https://github.com/Mjvolk3/torchcell/tree/main/src/torchcell/models/dcell.py
+# Test file: src/torchcell/models/test_dcell.py
+
+
+if __name__ == "__main__":
+    pass

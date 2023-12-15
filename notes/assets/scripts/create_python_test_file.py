6387af98c45ec0da091e2dfd589f97c024ce6b69
mjvolk3 <mjvolk3>
2023-12-15 00:27:31 -0600

    fixed file moving, broken from before

--- notes/assets/scripts/create_python_test_file.py
+++ notes/assets/scripts/create_python_test_file.py
@@ -17,42 +17,34 @@ WORKSPACE_DIR = os.getenv("WORKSPACE_DIR")
 def create_test_file(src_file_path):
     print('=' * 80)
     # Get the relative path from the source directory to WORKSPACE_DIR
-    src_directory = os.path.dirname(src_file_path)
-    rel_path_from_workspace_to_src_dir = os.path.relpath(src_directory, WORKSPACE_DIR)
+    rel_path_from_workspace_to_src_file = os.path.relpath(src_file_path, WORKSPACE_DIR)
 
     print(
-        f"Relative path from workspace to src directory: {rel_path_from_workspace_to_src_dir}"
+        f"Relative path from workspace to source file: {rel_path_from_workspace_to_src_file}"
     )
 
-    # Form new path replacing src with tests
-    test_directory = (
-        Path(rel_path_from_workspace_to_src_dir).as_posix().replace("src", "tests")
-    )
-    test_directory = os.path.join(WORKSPACE_DIR, test_directory)
-
-    print(f"Test directory after forming new path: {test_directory}")
-
-    # Extract the base filename, prepend "test_", and then reconstruct the path
-    base_filename = os.path.basename(src_file_path)
-    test_filename = "test_" + base_filename
+    # Form new path using PYTHON_PKG_TEST_REL_PATH
+    test_file_relative_path = rel_path_from_workspace_to_src_file.replace(PYTHON_PKG_REL_PATH, PYTHON_PKG_TEST_REL_PATH)
 
     # Construct the full path of the test file
-    test_file_path = os.path.join(test_directory, test_filename)
+    test_file_path = os.path.join(WORKSPACE_DIR, test_file_relative_path)
+    test_file_directory = os.path.dirname(test_file_path)
+    test_filename = "test_" + os.path.basename(src_file_path)
+    test_file_path = os.path.join(test_file_directory, test_filename)
+
+    print(f"Test file path: {test_file_path}")
 
     # If the test file doesn't exist, create it
     if not os.path.exists(test_file_path):
-        os.makedirs(os.path.dirname(test_file_path), exist_ok=True)
+        os.makedirs(test_file_directory, exist_ok=True)
         with open(test_file_path, "w") as test_file:
             test_file.write("# Test file\n")
         print(f"Test file created at: {test_file_path}")
-        subprocess.run(
-            [VSCODE_PATH, test_file_path]
-        )  # Open the created test file in VSCode
     else:
         print(f"Test file already exists at: {test_file_path}")
-        subprocess.run(
-            [VSCODE_PATH, test_file_path]
-        )  # Open the existing test file in VSCode
+
+    # Open the test file in VSCode
+    subprocess.run([VSCODE_PATH, test_file_path])
     print('=' * 80)
 
 if __name__ == "__main__":

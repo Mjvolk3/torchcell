4d10292350041a1007a43bc31157557c46027d89
mjvolk3 <mjvolk3>
2024-02-13 18:34:18 -0600

    BREAKING CHANGE: semantic release

--- torchcell/__version__.py
+++ torchcell/__version__.py
@@ -3,4 +3,4 @@
 # https://github.com/Mjvolk3/torchcell/tree/main/torchcell/__version__.py
 # Test file: tests/torchcell/test___version__.py
 
-__version__ = "0.0.34"  # Linked to bumpver in toml
+__version__ = "1.0.0"  # Linked to bumpver in toml

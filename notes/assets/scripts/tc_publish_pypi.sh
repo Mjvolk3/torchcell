097b44f95f514e4e9a67ca73bd6373e2438ed8fe
mjvolk3 <mjvolk3>
2024-02-14 00:06:04 -0600

    fix: tc_publish_pypi code action, partial automation

--- notes/assets/scripts/tc_publish_pypi.sh
+++ notes/assets/scripts/tc_publish_pypi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-rm -rf dist
+cd /Users/michaelvolk/Documents/projects/torchcell
+rm -rf ./dist
+eval "$(conda shell.bash hook)"
 conda activate torchcell
 python -m build
 twine upload dist/*

506cef5a8798c468ccdbec7df4792d0bd79a3c3c
mjvolk3 <mjvolk3>
2024-02-21 00:40:29 -0600

    fixed interactive mode, DmfKuzmin2018Adapter test

--- torchcell/knowledge_graphs/create_scerevisiae_kg_small.py
+++ torchcell/knowledge_graphs/create_scerevisiae_kg_small.py
@@ -59,18 +59,18 @@ def main() -> str:
         #     ),
         #     num_workers=mp.cpu_count(),
         # ),
-        SmfKuzmin2018Adapter(
-            dataset=SmfKuzmin2018Dataset(
-                root=osp.join(DATA_ROOT, "data/torchcell/smf_kuzmin2018")
-            ),
-            num_workers=mp.cpu_count(),
-        ),
-        # DmfKuzmin2018Adapter(
-        #     dataset=DmfKuzmin2018Dataset(
-        #         root=osp.join(DATA_ROOT, "data/torchcell/dmf_kuzmin2018")
+        # SmfKuzmin2018Adapter(
+        #     dataset=SmfKuzmin2018Dataset(
+        #         root=osp.join(DATA_ROOT, "data/torchcell/smf_kuzmin2018")
         #     ),
         #     num_workers=mp.cpu_count(),
         # ),
+        DmfKuzmin2018Adapter(
+            dataset=DmfKuzmin2018Dataset(
+                root=osp.join(DATA_ROOT, "data/torchcell/dmf_kuzmin2018")
+            ),
+            num_workers=mp.cpu_count(),
+        ),
         # TmfKuzmin2018Adapter(
         #     dataset=TmfKuzmin2018Dataset(
         #         root=osp.join(DATA_ROOT, "data/torchcell/tmf_kuzmin2018")

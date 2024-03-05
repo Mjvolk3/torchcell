bcfa141dfeec9cf99761a549cc4a8821bea86162
mjvolk3 <mjvolk3>
2024-03-04 19:09:39 -0600

    costanzo adapter working

--- torchcell/adapters/__init__.py
+++ torchcell/adapters/__init__.py
@@ -4,12 +4,19 @@ from .kuzmin2018_adapter import (
     DmfKuzmin2018Adapter,
     TmfKuzmin2018Adapter,
 )
+from .cell_adapter import CellAdapter
 
+cell_adapters = ["CellAdapter"]
+
+# Experiment Adapters
+
+# Dataset Adapters
 costanzo2016_adapters = ["SmfCostanzo2016Adapter", "DmfCostanzo2016Adapter"]
+
 kuzmin2018_adapters = [
     "SmfKuzmin2018Adapter",
     "DmfKuzmin2018Adapter",
     "TmfKuzmin2018Adapter",
 ]
 
-__all__ = costanzo2016_adapters + kuzmin2018_adapters
+__all__ = cell_adapters + costanzo2016_adapters + kuzmin2018_adapters

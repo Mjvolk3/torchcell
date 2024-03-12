dd2c0f5989fff2d94eac4eece6d01e3817fd0208
mjvolk3 <mjvolk3@illinois.edu>
2024-03-12 04:47:00 -0500

    db build scripts

---
+++ database/build/build_linux-amd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Activate Conda environment
+source ~/.bashrc
+conda activate myenv
+
+# Uninstall and reinstall your packages
+python -m pip uninstall torchcell -y
+python -m pip install git+https://github.com/Mjvolk3/torchcell.git@main
+
+python -m pip uninstall biocypher -y
+python -m pip install git+https://github.com/Mjvolk3/biocypher@main
+
+# Echo a message
+echo "----------------NOW_BUILDING_GRAPHS----------------"
+
+# Run your Python script and follow-up commands
+python -m torchcell.knowledge_graphs.create_scerevisiae_kg_small
+bash_script_path=$(cat logs/biocypher_file_name.log)
+cd /var/lib/neo4j
+chmod +x "${bash_script_path}"
+/bin/bash -c "${bash_script_path}"
+

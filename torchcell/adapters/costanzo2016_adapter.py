858fb00a499d1211bd167cf424930453a6477d9e
mjvolk3 <mjvolk3>
2024-03-26 15:36:52 -0500

    dmf adapter reverted to same as all others test.

--- torchcell/adapters/costanzo2016_adapter.py
+++ torchcell/adapters/costanzo2016_adapter.py
@@ -19,6 +19,8 @@ from torchcell.adapters.cell_adapter import CellAdapter
 
 # logging
 # Get the biocypher logger
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 logger = get_logger("biocypher")
 logger.setLevel(logging.ERROR)
 
@@ -60,183 +62,228 @@ class DmfCostanzo2016Adapter(CellAdapter):
         self.chunk_size = chunk_size
         self.loader_batch_size = loader_batch_size
 
-    def get_nodes(self):
-        yield from self._get_experiment_reference_nodes()
-        yield from self._get_genome_nodes()
-        yield from self.get_data_by_type(self._experiment_node)
-        yield from self.get_data_by_type(self._genotype_node)
-        yield from self.get_data_by_type(self._perturbation_node)
-        yield from self.get_data_by_type(self._environment_node)
-        # yield from self._get_reference_environment_nodes()
-        # OPTIMIZED
-        yield from self._get_environment_nodes()
-        # yield from self.get_data_by_type(self._media_node)
-        # OPTIMIZED
-        yield from self._get_media_nodes()
-        yield from self._get_reference_media_nodes()
-        # yield from self.get_data_by_type(self._temperature_node)
-        # OPTIMIZED
-        self._get_temperature_nodes()
-        yield from self._get_reference_temperature_nodes()
-        yield from self.get_data_by_type(self._phenotype_node)
-        yield from self._get_reference_phenotype_nodes()
-        yield from self.get_dataset_nodes()
 
-    def _get_environment_nodes(self) -> list[BioCypherNode]:
-        # HACK - we know we can loop ref for this node type
-        nodes = []
-        seen_node_ids = set()
-        for data in tqdm(self.dataset.experiment_reference_index):
-            environment_id = hashlib.sha256(
-                json.dumps(data.reference.reference_environment.model_dump()).encode(
-                    "utf-8"
-                )
-            ).hexdigest()
-            if environment_id not in seen_node_ids:
-                seen_node_ids.add(environment_id)
-                media = json.dumps(
-                    data.reference.reference_environment.media.model_dump()
-                )
-                node = BioCypherNode(
-                    node_id=environment_id,
-                    preferred_id=f"environment_ref_{environment_id}",
-                    node_label="environment",
-                    properties={
-                        "temperature": data.reference.reference_environment.temperature.value,
-                        "media": media,
-                        "serialized_data": json.dumps(
-                            data.reference.reference_environment.model_dump()
-                        ),
-                    },
-                )
-                nodes.append(node)
-        return nodes
+# class DmfCostanzo2016Adapter(CellAdapter):
+#     def __init__(
+#         self,
+#         dataset: DmfCostanzo2016Dataset,
+#         compute_workers: int,
+#         io_workers: int,
+#         chunk_size: int = int(1e4),
+#         loader_batch_size: int = int(1e3),
+#     ):
+#         super().__init__(
+#             dataset, compute_workers, io_workers, chunk_size, loader_batch_size
+#         )
+#         self.dataset = dataset
+#         self.compute_workers = compute_workers
+#         self.io_workers = io_workers
+#         self.chunk_size = chunk_size
+#         self.loader_batch_size = loader_batch_size
 
-    def _get_media_nodes(self) -> list[BioCypherNode]:
-        # HACK - we know we can loop ref for this node type
-        seen_node_ids = set()
-        nodes = []
-        for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
-            media_id = hashlib.sha256(
-                json.dumps(
-                    data.reference.reference_environment.media.model_dump()
-                ).encode("utf-8")
-            ).hexdigest()
-            if media_id not in seen_node_ids:
-                seen_node_ids.add(media_id)
-                name = data.reference.reference_environment.media.name
-                state = data.reference.reference_environment.media.state
-                node = BioCypherNode(
-                    node_id=media_id,
-                    preferred_id=f"media_{media_id}",
-                    node_label="media",
-                    properties={
-                        "name": name,
-                        "state": state,
-                        "serialized_data": json.dumps(
-                            data.reference.reference_environment.media.model_dump()
-                        ),
-                    },
-                )
-                nodes.append(node)
-        return nodes
+#     def get_nodes(self):
 
-    def _get_temperature_nodes(self) -> list[BioCypherNode]:
-        # HACK - we know we can loop ref for this node type
-        seen_node_ids = set()
-        nodes = []
-        for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
-            temperature_id = hashlib.sha256(
-                json.dumps(
-                    data.reference.reference_environment.temperature.model_dump()
-                ).encode("utf-8")
-            ).hexdigest()
-            if temperature_id not in seen_node_ids:
-                seen_node_ids.add(temperature_id)
-                node = BioCypherNode(
-                    node_id=temperature_id,
-                    preferred_id=f"temperature_{temperature_id}",
-                    node_label="temperature",
-                    properties={
-                        "value": data.reference.reference_environment.temperature.value,
-                        "unit": data.reference.reference_environment.temperature.unit,
-                        "serialized_data": json.dumps(
-                            data.reference.reference_environment.temperature.model_dump()
-                        ),
-                    },
-                )
-                nodes.append(node)
-        return nodes
+#         log.info("Running: self._get_experiment_reference_nodes()")
+#         yield from self._get_experiment_reference_nodes()
+#         log.info("Running: self._get_genome_nodes()")
+#         yield from self._get_genome_nodes()
+#         log.info("Running: self.get_data_by_type(self._experiment_node)")
+#         yield from self.get_data_by_type(self._experiment_node)
+#         log.info("Running: self.get_data_by_type(self._genotype_node)")
+#         yield from self.get_data_by_type(self._genotype_node)
+#         log.info("Running: self.get_data_by_type(self._perturbation_node)")
+#         yield from self.get_data_by_type(self._perturbation_node)
+#         log.info("Running: self._get_environment_nodes()")
+#         yield from self._get_environment_nodes()
+#         log.info("Running: self._get_reference_environment_nodes()")
+#         yield from self._get_reference_environment_nodes()
+#         # OPTIMIZED
+#         # yield from self.get_data_by_type(self._media_node)
+#         # OPTIMIZED
+#         log.info("Running: self._get_media_nodes()")
+#         yield from self._get_media_nodes()
+#         log.info("Running: self._get_reference_media_nodes()")
+#         yield from self._get_reference_media_nodes()
+#         # yield from self.get_data_by_type(self._temperature_node)
+#         # OPTIMIZED
+#         log.info("Running: self._get_temperature_nodes()")
+#         yield from self._get_temperature_nodes()
+#         log.info("Running: self._get_reference_temperature_nodes()")
+#         yield from self._get_reference_temperature_nodes()
+#         log.info("Running: self.get_data_by_type(self._phenotype_node)")
+#         yield from self.get_data_by_type(self._phenotype_node)
+#         log.info("Running: self._get_reference_phenotype_nodes()")
+#         yield from self._get_reference_phenotype_nodes()
+#         log.info("Running: self._get_dataset_nodes()")
+#         yield from self.get_dataset_nodes()
 
-    def get_edges(self):
-        yield from self.get_reference_dataset_edges()
-        yield from self.get_data_by_type(self._experiment_dataset_edge)
-        yield from self._get_reference_experiment_edges()
-        yield from self.get_data_by_type(self._genotype_experiment_edge)
-        yield from self.get_data_by_type(self._perturbation_genotype_edges)
-        yield from self.get_data_by_type(self._environment_experiment_edges)
-        yield from self._get_environment_experiment_ref_edges()
-        yield from self.get_data_by_type(self._phenotype_experiment_edges)
-        # yield from self.get_data_by_type(self._media_environment_edge)  # OPTIMIZED
-        yield from self._get_media_environment_edges()
-        # yield from self.get_data_by_type(self._temperature_environment_edge)  
-        # OPTIMIZED
-        yield from self._get_temperature_environment_edges()
-        yield from self._get_genome_edges()  # KEEP
+#     def _get_environment_nodes(self) -> list[BioCypherNode]:
+#         # HACK - we know we can loop ref for this node type
+#         nodes = []
+#         seen_node_ids = set()
+#         for data in tqdm(self.dataset.experiment_reference_index):
+#             environment_id = hashlib.sha256(
+#                 json.dumps(data.reference.reference_environment.model_dump()).encode(
+#                     "utf-8"
+#                 )
+#             ).hexdigest()
+#             if environment_id not in seen_node_ids:
+#                 seen_node_ids.add(environment_id)
+#                 media = json.dumps(
+#                     data.reference.reference_environment.media.model_dump()
+#                 )
+#                 node = BioCypherNode(
+#                     node_id=environment_id,
+#                     preferred_id="environment",
+#                     node_label="environment",
+#                     properties={
+#                         "temperature": data.reference.reference_environment.temperature.value,
+#                         "media": media,
+#                         "serialized_data": json.dumps(
+#                             data.reference.reference_environment.model_dump()
+#                         ),
+#                     },
+#                 )
+#                 nodes.append(node)
+#         return nodes
 
-    def _get_media_environment_edges(self) -> list[BioCypherEdge]:
-        # HACK Optimized by using reference
-        # We know reference contains all media and envs
-        edges = []
-        seen_media_environment_pairs: Set[tuple] = set()
-        for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
-            environment_id = hashlib.sha256(
-                json.dumps(data.reference.reference_environment.model_dump()).encode(
-                    "utf-8"
-                )
-            ).hexdigest()
-            media_id = hashlib.sha256(
-                json.dumps(
-                    data.reference.reference_environment.media.model_dump()
-                ).encode("utf-8")
-            ).hexdigest()
-            media_environment_pair = (media_id, environment_id)
-            if media_environment_pair not in seen_media_environment_pairs:
-                seen_media_environment_pairs.add(media_environment_pair)
-                edge = BioCypherEdge(
-                    source_id=media_id,
-                    target_id=environment_id,
-                    relationship_label="media member of",
-                )
-                edges.append(edge)
-        return edges
+#     def _get_media_nodes(self) -> list[BioCypherNode]:
+#         # HACK - we know we can loop ref for this node type
+#         seen_node_ids = set()
+#         nodes = []
+#         for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
+#             media_id = hashlib.sha256(
+#                 json.dumps(
+#                     data.reference.reference_environment.media.model_dump()
+#                 ).encode("utf-8")
+#             ).hexdigest()
+#             if media_id not in seen_node_ids:
+#                 seen_node_ids.add(media_id)
+#                 name = data.reference.reference_environment.media.name
+#                 state = data.reference.reference_environment.media.state
+#                 node = BioCypherNode(
+#                     node_id=media_id,
+#                     preferred_id="media",
+#                     node_label="media",
+#                     properties={
+#                         "name": name,
+#                         "state": state,
+#                         "serialized_data": json.dumps(
+#                             data.reference.reference_environment.media.model_dump()
+#                         ),
+#                     },
+#                 )
+#                 nodes.append(node)
+#         return nodes
 
-    def _get_temperature_environment_edges(self) -> list[BioCypherEdge]:
-        # HACK Optimized by using reference
-        # We know reference contain all envs and temps
-        edges = []
-        seen_temperature_environment_pairs: Set[tuple] = set()
-        for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
-            environment_id = hashlib.sha256(
-                json.dumps(data.reference.reference_environment.model_dump()).encode(
-                    "utf-8"
-                )
-            ).hexdigest()
-            temperature_id = hashlib.sha256(
-                json.dumps(
-                    data.reference.reference_environment.temperature.model_dump()
-                ).encode("utf-8")
-            ).hexdigest()
-            temperature_environment_pair = (temperature_id, environment_id)
-            if temperature_environment_pair not in seen_temperature_environment_pairs:
-                seen_temperature_environment_pairs.add(temperature_environment_pair)
-                edge = BioCypherEdge(
-                    source_id=temperature_id,
-                    target_id=environment_id,
-                    relationship_label="temperature member of",
-                )
-                edges.append(edge)
-        return edges
+#     def _get_temperature_nodes(self) -> list[BioCypherNode]:
+#         # HACK - we know we can loop ref for this node type
+#         seen_node_ids = set()
+#         nodes = []
+#         for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
+#             temperature_id = hashlib.sha256(
+#                 json.dumps(
+#                     data.reference.reference_environment.temperature.model_dump()
+#                 ).encode("utf-8")
+#             ).hexdigest()
+#             if temperature_id not in seen_node_ids:
+#                 seen_node_ids.add(temperature_id)
+#                 node = BioCypherNode(
+#                     node_id=temperature_id,
+#                     preferred_id="temperature",
+#                     node_label="temperature",
+#                     properties={
+#                         "value": data.reference.reference_environment.temperature.value,
+#                         "unit": data.reference.reference_environment.temperature.unit,
+#                         "serialized_data": json.dumps(
+#                             data.reference.reference_environment.temperature.model_dump()
+#                         ),
+#                     },
+#                 )
+#                 nodes.append(node)
+#         return nodes
+
+#     def get_edges(self):
+#         log.info("Running: self.get_reference_dataset_edges()")
+#         yield from self.get_reference_dataset_edges()
+#         log.info("Running: self.get_data_by_type(self._experiment_dataset_edge)")
+#         yield from self.get_data_by_type(self._experiment_dataset_edge)
+#         log.info("Running: self._get_reference_experiment_edges()")
+#         yield from self._get_reference_experiment_edges()
+#         log.info("Running: self.get_data_by_type(self._genotype_experiment_edge)")
+#         yield from self.get_data_by_type(self._genotype_experiment_edge)
+#         log.info("Running: self.get_data_by_type(self._perturbation_genotype_edges)")
+#         yield from self.get_data_by_type(self._perturbation_genotype_edges)
+#         log.info("Running: self.get_data_by_type(self._environment_experiment_edges)")
+#         yield from self.get_data_by_type(self._environment_experiment_edges)
+#         log.info("Running: self._get_environment_experiment_ref_edges()")
+#         yield from self._get_environment_experiment_ref_edges()
+#         log.info("Running: self.get_data_by_type(self._phenotype_experiment_edges)")
+#         yield from self.get_data_by_type(self._phenotype_experiment_edges)
+#         # yield from self.get_data_by_type(self._media_environment_edge)  # OPTIMIZED
+#         log.info("Running: self._get_media_environment_edges()")
+#         yield from self._get_media_environment_edges()
+#         # yield from self.get_data_by_type(self._temperature_environment_edge)
+#         # OPTIMIZED
+#         log.info("Running: self._get_temperature_environment_edges()")
+#         yield from self._get_temperature_environment_edges()
+#         log.info("Running: self._get_genome_edges()")
+#         yield from self._get_genome_edges()  # KEEP
+#         log.info("Finished: get_edges")
+
+#     def _get_media_environment_edges(self) -> list[BioCypherEdge]:
+#         # HACK Optimized by using reference
+#         # We know reference contains all media and envs
+#         edges = []
+#         seen_media_environment_pairs: Set[tuple] = set()
+#         for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
+#             environment_id = hashlib.sha256(
+#                 json.dumps(data.reference.reference_environment.model_dump()).encode(
+#                     "utf-8"
+#                 )
+#             ).hexdigest()
+#             media_id = hashlib.sha256(
+#                 json.dumps(
+#                     data.reference.reference_environment.media.model_dump()
+#                 ).encode("utf-8")
+#             ).hexdigest()
+#             media_environment_pair = (media_id, environment_id)
+#             if media_environment_pair not in seen_media_environment_pairs:
+#                 seen_media_environment_pairs.add(media_environment_pair)
+#                 edge = BioCypherEdge(
+#                     source_id=media_id,
+#                     target_id=environment_id,
+#                     relationship_label="media member of",
+#                 )
+#                 edges.append(edge)
+#         return edges
+
+#     def _get_temperature_environment_edges(self) -> list[BioCypherEdge]:
+#         # HACK Optimized by using reference
+#         # We know reference contain all envs and temps
+#         edges = []
+#         seen_temperature_environment_pairs: Set[tuple] = set()
+#         for i, data in tqdm(enumerate(self.dataset.experiment_reference_index)):
+#             environment_id = hashlib.sha256(
+#                 json.dumps(data.reference.reference_environment.model_dump()).encode(
+#                     "utf-8"
+#                 )
+#             ).hexdigest()
+#             temperature_id = hashlib.sha256(
+#                 json.dumps(
+#                     data.reference.reference_environment.temperature.model_dump()
+#                 ).encode("utf-8")
+#             ).hexdigest()
+#             temperature_environment_pair = (temperature_id, environment_id)
+#             if temperature_environment_pair not in seen_temperature_environment_pairs:
+#                 seen_temperature_environment_pairs.add(temperature_environment_pair)
+#                 edge = BioCypherEdge(
+#                     source_id=temperature_id,
+#                     target_id=environment_id,
+#                     relationship_label="temperature member of",
+#                 )
+#                 edges.append(edge)
+#         return edges
 
 
 if __name__ == "__main__":

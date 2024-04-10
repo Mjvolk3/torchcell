0269bf2c8ce7a3b7534a2b480deeba46e43d78e6
mjvolk3 <mjvolk3>
2024-04-10 12:52:25 -0500

    mlp reparameterized.

--- torchcell/models/mlp.py
+++ torchcell/models/mlp.py
@@ -6,7 +6,7 @@
 import torch
 import torch.nn as nn
 from torchcell.models import act_register
-
+from typing import Optional
 
 class Mlp(nn.Module):
     def __init__(
@@ -16,37 +16,43 @@ class Mlp(nn.Module):
         out_channels: int,
         num_layers: int,
         dropout_prob: float = 0.0,
-        norm: str = "none",
-        activation: str = "relu",
-        output_activation: str | None = None,
+        norm: Optional[str] = None,
+        activation: Optional[str] = None,
+        output_activation: Optional[str] = None,
     ):
         super().__init__()
+        assert norm in [None, "batch", "instance", "layer"], "Invalid norm type"
+        assert activation in [None] + list(
+            act_register.keys()
+        ), "Invalid activation type"
 
-        assert norm in ["none", "batch", "instance", "layer"], "Invalid norm type"
-        assert activation in act_register.keys(), "Invalid activation type"
-
-        def create_block(in_dim, out_dim, norm, activation):
+        def create_block(
+            in_dim: int, out_dim: int, norm: Optional[str], activation: Optional[str]
+        ) -> nn.Sequential:
             block = [nn.Linear(in_dim, out_dim)]
-            if norm != "none":
+            if norm:
                 if norm == "batch":
                     block.append(nn.BatchNorm1d(out_dim))
                 elif norm == "instance":
                     block.append(nn.InstanceNorm1d(out_dim, affine=True))
                 elif norm == "layer":
                     block.append(nn.LayerNorm(out_dim))
-            block.append(act_register[activation])
+            if activation:
+                block.append(act_register[activation])
             return nn.Sequential(*block)
 
         layers = []
         for i in range(num_layers):
-            if i == 0:
+            if num_layers == 1:
+                # Directly map from in_channels to out_channels for a single-layer model
+                layers.append(create_block(in_channels, out_channels, norm, activation))
+                break
+            elif i == 0:
                 layers.append(
                     create_block(in_channels, hidden_channels, norm, activation)
                 )
             elif i == num_layers - 1:
-                layers.append(
-                    create_block(hidden_channels, out_channels, norm, activation)
-                )
+                layers.append(create_block(hidden_channels, out_channels, None, None))
                 layers.append(nn.Dropout(dropout_prob))
             else:
                 layers.append(
@@ -58,7 +64,7 @@ class Mlp(nn.Module):
 
         self.model = nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x).squeeze(-1)
 
 
@@ -69,12 +75,12 @@ if __name__ == "__main__":
 
     # Define a fake model
     in_channels = input_dim
-    hidden_channels = 128
+    hidden_channels = 0
     out_channels = 10
-    num_layers = 4
+    num_layers = 1
     dropout_prob = 0.2
-    norm = "none"
-    activation = "relu"
+    norm = None
+    activation = None
     output_activation = "sigmoid"
 
     model = Mlp(

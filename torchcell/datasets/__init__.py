9eab5aa0a1e6fdd947e632e1a0cbc5c44f0ec975
mjvolk3 <mjvolk3>
2024-07-17 21:59:15 -0500

    data registry working

--- torchcell/datasets/__init__.py
+++ torchcell/datasets/__init__.py
@@ -2,11 +2,21 @@
 # [[torchcell.datasets.__init__]]
 # https://github.com/Mjvolk3/torchcell/tree/main/torchcell/datasets/__init__.py
 # Test file: tests/torchcell/datasets/test___init__.py
-from .codon_frequency import CodonFrequencyDataset
-# TODO when we import this we get all sorts of import error
-# from .dcell import DCellDataset  
 
-# from .costanzo2016 import DmfCostanzo2016Dataset, SmfCostanzo2016Dataset # in scerevisiae
+from .codon_frequency import CodonFrequencyDataset
+
+# TODO when we import this we get all sorts of import error
+# from .dcell import DCellDataset
+
+# scerevisiae datasets
+from .scerevisiae.costanzo2016 import SmfCostanzo2016Dataset, DmfCostanzo2016Dataset
+from .scerevisiae.kuzmin2018 import (
+    SmfKuzmin2018Dataset,
+    DmfKuzmin2018Dataset,
+    TmfKuzmin2018Dataset,
+)
+
+# other datasets
 from .fungal_up_down_transformer import FungalUpDownTransformerDataset
 from .nucleotide_transformer import NucleotideTransformerDataset
 from .one_hot_gene import OneHotGeneDataset
@@ -16,7 +26,11 @@ from .esm2 import Esm2Dataset
 from .codon_language_model import CalmDataset
 from .random_embedding import RandomEmbeddingDataset
 
+from .dataset_registry import dataset_registry
+
 core_datasets = ["DCellDataset"]
+
+scerevisiae_datasets = ["DmfCostanzo2016Dataset", "SmfCostanzo2016Dataset"]
 
 embedding_datasets = [
     "NucleotideTransformerDataset",
@@ -27,7 +41,7 @@ embedding_datasets = [
     "GraphEmbeddingDataset",
     "Esm2Dataset",
     "CalmDataset",
-    "RandomEmbeddingDataset"
+    "RandomEmbeddingDataset",
 ]
 
 

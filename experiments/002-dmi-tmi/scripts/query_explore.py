1f3dd1a2f5fb86d8d569360b7162a5ed768e9dfc
mjvolk3 <mjvolk3>
2024-08-10 23:23:41 -0500

    adjust typing info on Deduplicator

---
+++ experiments/002-dmi-tmi/scripts/query_explore.py
@@ -0,0 +1,77 @@
+from torchcell.data import Neo4jCellDataset, ExperimentDeduplicator
+import os.path as osp
+from dotenv import load_dotenv
+from torchcell.graph import SCerevisiaeGraph
+from torchcell.datamodules import CellDataModule
+import os
+import json
+from torchcell.sequence.genome.scerevisiae.s288c import SCerevisiaeGenome
+from torchcell.datasets.fungal_up_down_transformer import FungalUpDownTransformerDataset
+from tqdm import tqdm
+
+
+def main():
+
+    load_dotenv()
+    DATA_ROOT = os.getenv("DATA_ROOT")
+    with open("experiments/001-dmi-tmi/query.cql", "r") as f:
+        query = f.read()
+
+    ### Add Embeddings
+    genome = SCerevisiaeGenome(osp.join(DATA_ROOT, "data/sgd/genome"))
+    genome.drop_chrmt()
+    genome.drop_empty_go()
+
+    with open("gene_set.json", "w") as f:
+        json.dump(list(genome.gene_set), f)
+
+    graph = SCerevisiaeGraph(
+        data_root=osp.join(DATA_ROOT, "data/sgd/genome"), genome=genome
+    )
+    fudt_3prime_dataset = FungalUpDownTransformerDataset(
+        root="data/scerevisiae/fudt_embedding",
+        genome=genome,
+        model_name="species_downstream",
+    )
+    fudt_5prime_dataset = FungalUpDownTransformerDataset(
+        root="data/scerevisiae/fudt_embedding",
+        genome=genome,
+        model_name="species_downstream",
+    )
+    
+    # TODO will have to change as this was build for fitness.
+    deduplicator = ExperimentDeduplicator()
+    dataset_root = osp.join(DATA_ROOT, "data/torchcell/experiments/002-dmi-tmi")
+    dataset = Neo4jCellDataset(
+        root=dataset_root,
+        query=query,
+        genome=genome,
+        graphs={"physical": graph.G_physical, "regulatory": graph.G_regulatory},
+        node_embeddings={
+            "fudt_3prime": fudt_3prime_dataset,
+            "fudt_5prime": fudt_5prime_dataset,
+        },
+        deduplicator=deduplicator,
+        max_size=int(1e2),
+    )
+    print(len(dataset))
+    # Data module testing
+
+    data_module = CellDataModule(
+        dataset=dataset,
+        cache_dir=osp.join(dataset_root, "data_module_cache"),
+        batch_size=8,
+        random_seed=42,
+        num_workers=4,
+        pin_memory=False,
+    )
+    data_module.setup()
+    for batch in tqdm(data_module.all_dataloader()):
+        pass
+        print()
+
+    print("finished")
+
+
+if __name__ == "__main__":
+    main()

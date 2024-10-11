d30b148c8f4a53bf9c9f9d2beedb8d5c41c714e0
Michael Volk <mjvolk3@illinois.edu>
2024-10-11 04:49:18 -0500

    gat diff pool

--- experiments/003-fit-int/scripts/gat_diffpool.py
+++ experiments/003-fit-int/scripts/gat_diffpool.py
@@ -442,7 +442,7 @@ def main(cfg: DictConfig) -> None:
     trainer = L.Trainer(
         strategy=wandb.config.trainer["strategy"],
         accelerator=wandb.config.trainer["accelerator"],
-        devices=devices,
+        devices=1, # FLAG
         logger=wandb_logger,
         max_epochs=wandb.config.trainer["max_epochs"],
         callbacks=[checkpoint_callback],

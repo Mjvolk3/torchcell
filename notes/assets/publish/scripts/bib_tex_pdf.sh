e973781bd0b7934d600d63ca5ddfe34ac8f94381
mjvolk3 <mjvolk3>
2025-04-02 22:22:42 -0500

    update bibtex output

--- notes/assets/publish/scripts/bib_tex_pdf.sh
+++ notes/assets/publish/scripts/bib_tex_pdf.sh
@@ -7,7 +7,32 @@ header_includes_path="${output_dir}/assets/publish/tex-templates/header-includes
 
 echo "Edit notes/assets/publish/tex-templates/header-includes.tex for customizing spacing."
 
-cd ./notes && pandoc -F mermaid-filter -s "${input_file}" -o "${output_dir}/assets/pdf-output/${output_filename}.pdf" --pdf-engine=xelatex --citeproc --bibliography assets/publish/bib/bib.bib --metadata csl=assets/publish/bib/nature.csl -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' --include-in-header="${header_includes_path}" --strip-comments --dpi=600 && cd ..
+# cd ./notes && pandoc -F mermaid-filter -s "${input_file}" -o "${output_dir}/assets/pdf-output/${output_filename}.pdf" --pdf-engine=xelatex --citeproc --bibliography assets/publish/bib/bib.bib --metadata csl=assets/publish/bib/nature.csl -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' --include-in-header="${header_includes_path}" --strip-comments --dpi=600 && cd ..
+
+cd ./notes && pandoc -F mermaid-filter \
+  --metadata link-citations=true \
+  -s "${input_file}" \
+  -o "${output_dir}/assets/pdf-output/${output_filename}.pdf" \
+  --pdf-engine=xelatex \
+  --citeproc \
+  --bibliography assets/publish/bib/bib.bib \
+  --metadata csl=assets/publish/bib/nature.csl \
+  -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' \
+  --include-in-header="${header_includes_path}" \
+  --strip-comments --dpi=600 && cd ..
+
+# cd ./notes && pandoc -F mermaid-filter \
+#     -L assets/publish/filters/citations-links.lua \
+#     -s "${input_file}" \
+#     -o "${output_dir}/assets/pdf-output/${output_filename}.pdf" \
+#     --pdf-engine=xelatex \
+#     --citeproc \
+#     --bibliography assets/publish/bib/bib.bib \
+#     --metadata csl=assets/publish/bib/nature_citation_links.csl \
+#     -V geometry:'top=2cm, bottom=1.5cm, left=2cm, right=2cm' \
+#     --include-in-header="${header_includes_path}" \
+#     --strip-comments \
+#     --dpi=600 && cd ..
 
 output_file_path="${output_dir}/assets/pdf-output/${output_filename}.pdf"
 echo "Output file: ${output_file_path}"

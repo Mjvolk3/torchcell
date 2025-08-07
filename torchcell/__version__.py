35bab1397bbffdb6b60e34dbbbd04ce215b9788e
semantic-release <semantic-release>
2025-08-07 03:40:39 +0000

    1.1.0
    Automatically generated by python-semantic-release

--- torchcell/__version__.py
+++ torchcell/__version__.py
@@ -3,4 +3,4 @@
 # https://github.com/Mjvolk3/torchcell/tree/main/torchcell/__version__.py
 # Test file: tests/torchcell/test___version__.py
 
-__version__ = "1.0.0"  # Linked to semantic-release in toml
+__version__ = "1.1.0"  # Linked to semantic-release in toml
